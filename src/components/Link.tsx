c2c57e8aefcaa4512ed0b5d35b00023214b7d575
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2022-12-18 08:56:41 +0100

    Fix props spread in link component

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -4,13 +4,13 @@ type LinkProps = {
   isActive?: boolean;
 } & HTMLProps<HTMLAnchorElement>;
 
-export const Link = (props: LinkProps) => {
+export const Link = ({ isActive, ...props }: LinkProps) => {
   return (
     <a
       {...props}
       className={`px-4 py-2 hover:bg-slate-100 rounded-md transition-all active:scale-95
         ${
-          props.isActive
+          isActive
             ? `text-blue-600 dark:text-blue-500
             dark:hover:text-blue-500 font-medium`
             : "dark:hover:text-slate-300"
