d891cab58936c510de8895d570aad85c0d79231e
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2022-12-19 17:09:42 +0100

    Style author link

--- src/components/Author.tsx
+++ src/components/Author.tsx
@@ -2,7 +2,11 @@ type AuthorProps = {};
 
 export const Author = ({}: AuthorProps) => {
   return (
-    <div className="flex flex-col gap-3">
+    <a
+      href="/about"
+      className="flex flex-col gap-3 rounded-xl
+    hover:bg-slate-100 dark:hover:bg-slate-800 p-4 -m-4 transition-all"
+    >
       <img
         alt="Tomasz Chmielnicki"
         src="/me.jpeg"
@@ -12,6 +16,6 @@ export const Author = ({}: AuthorProps) => {
         Tomasz Chmielnicki
       </h3>
       <p>Web developer, building things.</p>
-    </div>
+    </a>
   );
 };
