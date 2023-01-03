fc9622bf6f5404ab0e0913279d22022e18abe45e
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2023-01-03 19:33:38 +0100

    Style author component on mobile

--- src/components/Author.tsx
+++ src/components/Author.tsx
@@ -4,18 +4,20 @@ export const Author = ({}: AuthorProps) => {
   return (
     <a
       href="/about"
-      className="flex flex-col gap-3 rounded-xl
+      className="flex items-center lg:items-start lg:flex-col gap-4 lg:gap-3 rounded-xl
     hover:bg-slate-100 dark:hover:bg-slate-800 p-4 -m-4 transition-all"
     >
       <img
         alt="Tomasz Chmielnicki"
         src="/me.jpeg"
-        className="rounded-full w-24 h-24"
+        className="rounded-full w-14 h-14 lg:w-24 lg:h-24"
       />
-      <h3 className="text-black dark:text-white font-medium">
-        Tomasz Chmielnicki
-      </h3>
-      <p>Web developer, building things.</p>
+      <div>
+        <h3 className="text-black dark:text-white mb-2 font-medium text-lg lg:text-base">
+          Tomasz Chmielnicki
+        </h3>
+        <p>Web developer, building things.</p>
+      </div>
     </a>
   );
 };
