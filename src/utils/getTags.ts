802cbc1a90a0ea40167b16a024a6ffdba8689cc1
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2023-01-06 17:24:01 +0100

    Refactor reduce type

--- src/utils/getTags.ts
+++ src/utils/getTags.ts
@@ -2,10 +2,10 @@ import { toKebabCase } from "./toKebabCase";
 import type { Post } from "./types";
 
 export const getTags = (posts: Post[]) =>
-  posts.reduce(
+  posts.reduce<string[]>(
     (acc, { frontmatter: { tags } }) => [
       ...acc,
       ...tags.map(toKebabCase).filter((tag) => !acc.includes(tag)),
     ],
-    [] as string[]
+    []
   );
