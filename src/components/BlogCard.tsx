16acf99e979efe9cdcd6eaa3d2a41c43300f6cb6
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2023-01-08 21:28:06 +0100

    Handle posts with short titles in blog card

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -8,11 +8,11 @@ export const BlogCard = ({ readTime, title, url }: BlogCardProps) => {
   return (
     <a
       href={url}
-      className="block p-5 bg-slate-100 hover:bg-slate-200 rounded-xl
-      dark:bg-slate-800 dark:hover:bg-slate-700 transition-all"
+      className="p-5 bg-slate-100 hover:bg-slate-200 rounded-xl
+      dark:bg-slate-800 dark:hover:bg-slate-700 transition-all flex flex-col"
     >
       <h3 className="text-black dark:text-white mb-3 font-medium">{title}</h3>
-      <div className="text-sm">{readTime}</div>
+      <div className="text-sm mt-auto">{readTime}</div>
     </a>
   );
 };
