dd3699fafe58e0fa3fe7fde3db8a124a0acf0c0c
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2023-01-22 13:52:15 +0100

    Add icon to search input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,7 +5,7 @@ type InputProps = HTMLProps<HTMLInputElement>;
 export const Input = ({ className = "", ...props }: InputProps) => {
   return (
     <input
-      className={`w-full mb-8 py-2 px-3 rounded-md text-black border border-slate-200
+      className={`w-full py-2 px-3 rounded-md text-black border border-slate-200
       focus:border-transparent transition-all bg-transparent
       outline-0 focus:outline-2 outline outline-blue-500 hover:border-slate-400
       dark:border-slate-600 dark:focus:border-transparent dark:text-white
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,15 +1,20 @@
 import { useSearchQuery } from "../store/postsFilter";
+import { Search } from "./icons/Search";
 import { Input } from "./Input";
 
 export const SearchInput = () => {
   const [input, setInput] = useSearchQuery();
   return (
-    <Input
-      value={input}
-      onChange={(e) => {
-        setInput((e.target as HTMLInputElement).value);
-      }}
-      placeholder="Search"
-    />
+    <div className="relative mb-8">
+      <Search className="absolute left-3 top-1/2 -translate-y-1/2 -z-10" />
+      <Input
+        value={input}
+        onChange={(e) => {
+          setInput((e.target as HTMLInputElement).value);
+        }}
+        placeholder="Search"
+        className="pl-9"
+      />
+    </div>
   );
 };
