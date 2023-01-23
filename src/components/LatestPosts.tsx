3a109bcdb34c99b2f79a1222e31ca3a060b1c4cc
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2023-01-23 17:18:06 +0100

    Add missing key

--- src/components/LatestPosts.tsx
+++ src/components/LatestPosts.tsx
@@ -12,7 +12,7 @@ export const LatestPosts = ({ posts }: LatestPostsProps) => {
       <SecondaryHeading className="mb-6 sm:mb-8">Latest posts</SecondaryHeading>
       <div className="flex flex-col sm:grid grid-cols-3 gap-6 sm:gap-10">
         {posts.map(({ url, frontmatter: { title, minutesRead } }) => (
-          <BlogCard readTime={minutesRead} title={title} url={url} />
+          <BlogCard key={url} readTime={minutesRead} title={title} url={url} />
         ))}
       </div>
     </div>
